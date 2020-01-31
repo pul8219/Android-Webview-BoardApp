b6b1af2303ce2ae00468cd5520606b2ff16f23d2
ParkYurim <pul8219@naver.com>
2020-01-31 17:59:05 +0900

    modify MainActivity.java

--- app/src/main/java/com/example/yrboardapp/MainActivity.java
+++ app/src/main/java/com/example/yrboardapp/MainActivity.java
@@ -1,11 +1,37 @@
 package com.example.yrboardapp;
 
+//추가
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.Configuration;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static com.example.yrboardapp.ConstantUrl.URL;
 
@@ -13,35 +39,222 @@ import static com.example.yrboardapp.ConstantUrl.URL;
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
     private WebSettings webSettings;
-    private CustomChromeClient chromeClient;
-
-//    private DBHelper dbHelper;
-//    private SQLiteDatabase database;
+    //private CustomChromeClient chromeClient;
+    //    private DBHelper dbHelper;
+    //    private SQLiteDatabase database;
 
     private final static String MAIN_URL = URL+ "www/signIn.html";
 
+    //추가
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private final static int FCR = 1;
+    private String mCM;
+    private ValueCallback<Uri> mUM;
+    private ValueCallback<Uri[]> mUMA;
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            Uri[] results = null;
+
+            //Check if response is positive
+            if (resultCode == Activity.RESULT_OK) {
+                if (requestCode == FCR) {
+
+                    if (null == mUMA) {
+                        return;
+                    }
+                    if (intent == null) {
+                        //Capture Photo if no image available
+                        if (mCM != null) {
+                            results = new Uri[]{Uri.parse(mCM)};
+                        }
+                    } else {
+                        String dataString = intent.getDataString();
+                        if (dataString != null) {
+                            results = new Uri[]{Uri.parse(dataString)};
+                        }
+                    }
+                }
+            }
+            mUMA.onReceiveValue(results);
+            mUMA = null;
+        } else {
+
+            if (requestCode == FCR) {
+                if (null == mUM) return;
+                Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();
+                mUM.onReceiveValue(result);
+                mUM = null;
+            }
+        }
+    }
+
+    @SuppressLint({"SetJavaScriptEnabled", "WrongViewCast"}) //추가
     @Override
     protected void onCreate(Bundle savedInstanceState) { /* main함수 처럼 최우선적으로 실행되는 메서드*/
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       //dbHelper = new DBHelper(this); /*내장 DB를 생성*/
-       //database = dbHelper.getWritableDatabase();
+        //추가
+        if (Build.VERSION.SDK_INT >= 23 && (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, 1);
+        }
 
         webView = (WebView) findViewById(R.id.webvw); /* xml Java코드 연결 */
-        chromeClient = new CustomChromeClient(this);
+        assert webView != null; //추가
+        //chromeClient = new CustomChromeClient(this);
 
         webView.setWebViewClient(new WebViewClient()); // URL을 Web View에서 실행할 수 있게 하는 것(새 창 뜨지 않게)
-        webView.setWebChromeClient(chromeClient);
+        //webView.setWebChromeClient(chromeClient);
         webSettings = webView.getSettings();
 
         webSettings.setJavaScriptEnabled(true); /* javascript를 실행할 수 있게 */
         webView.addJavascriptInterface(new JSInterface(this, webView), "board");
+        webSettings.setAllowFileAccess(true); //추가
+
 
         webView.loadUrl(MAIN_URL);
+
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            webSettings.setMixedContentMode(0);
+            webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        } else if (Build.VERSION.SDK_INT >= 19) {
+            webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        } else if (Build.VERSION.SDK_INT < 19) {
+            webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        }
+
+        webView.setWebChromeClient(new WebChromeClient() {
+
+            //For Android 3.0+
+            public void openFileChooser(ValueCallback<Uri> uploadMsg) {
+
+                mUM = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("*/*");
+                MainActivity.this.startActivityForResult(Intent.createChooser(i, "File Chooser"), FCR);
+            }
+
+            // For Android 3.0+, above method not supported in some android 3+ versions, in such case we use this
+            public void openFileChooser(ValueCallback uploadMsg, String acceptType) {
+
+                mUM = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("*/*");
+                MainActivity.this.startActivityForResult(
+                        Intent.createChooser(i, "File Browser"),
+                        FCR);
+            }
+
+            //For Android 4.1+
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+
+                mUM = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("*/*");
+                MainActivity.this.startActivityForResult(Intent.createChooser(i, "File Chooser"), MainActivity.FCR);
+            }
+
+            //For Android 5.0+
+            public boolean onShowFileChooser(
+                    WebView webView, ValueCallback<Uri[]> filePathCallback,
+                    WebChromeClient.FileChooserParams fileChooserParams) {
+
+                if (mUMA != null) {
+                    mUMA.onReceiveValue(null);
+                }
+
+                mUMA = filePathCallback;
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(MainActivity.this.getPackageManager()) != null) {
+
+                    File photoFile = null;
+
+                    try {
+                        photoFile = createImageFile();
+                        takePictureIntent.putExtra("PhotoPath", mCM);
+                    } catch (IOException ex) {
+                        Log.e(TAG, "Image file creation failed", ex);
+                    }
+                    if (photoFile != null) {
+                        mCM = "file:" + photoFile.getAbsolutePath();
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                    } else {
+                        takePictureIntent = null;
+                    }
+                }
+
+                Intent contentSelectionIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                contentSelectionIntent.addCategory(Intent.CATEGORY_OPENABLE);
+                contentSelectionIntent.setType("*/*");
+                Intent[] intentArray;
+
+                if (takePictureIntent != null) {
+                    intentArray = new Intent[]{takePictureIntent};
+                } else {
+                    intentArray = new Intent[0];
+                }
+
+                Intent chooserIntent = new Intent(Intent.ACTION_CHOOSER);
+                chooserIntent.putExtra(Intent.EXTRA_INTENT, contentSelectionIntent);
+                chooserIntent.putExtra(Intent.EXTRA_TITLE, "Image Chooser");
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray);
+                startActivityForResult(chooserIntent, FCR);
+
+                return true;
+            }
+        });
+
+
+    } //onCreate() end
+
+    // Create an image file
+    private File createImageFile() throws IOException {
+
+        @SuppressLint("SimpleDateFormat") String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "img_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        return File.createTempFile(imageFileName, ".jpg", storageDir);
     }
 
+    @Override
+    public boolean onKeyDown(int keyCode, @NonNull KeyEvent event) {
+
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+
+                    if (webView.canGoBack()) {
+                        webView.goBack();
+                    } else {
+                        finish();
+                    }
+
+                    return true;
+            }
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    public class Callback extends WebViewClient {
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+            Toast.makeText(getApplicationContext(), "Failed loading app!", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     // 수정 필요
     @Override
@@ -55,25 +268,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-//    public class JavascriptInfoBoard{
-//
-//        public JavascriptInfoBoard(){
-//
-//        }
-//        @JavascriptInterface
-//        public void joinUs(String id, String name, String tel, String birth, String gender, String password){
-//            MemberVO memberVo = new MemberVO();
-//            memberVo.setId(id);
-//            memberVo.setName(name);
-//            memberVo.setCpnum(tel);
-//            memberVo.setBirth(birth);
-//            memberVo.setGender(gender);
-//            memberVo.setPw(password);
-//// 아래 문장 주석이었음
-////            dbHelper.joinClients(memberVo,database);
-//        }
-//
-//    }
+
 
 
 
